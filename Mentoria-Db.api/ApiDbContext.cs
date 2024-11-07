3fe09a90e48b832a4b1dd2df381cc9a30ce51929
Rafael <rafaelmatos212@gmail.com>
2024-11-07 13:50:26 -0300

    First commit

---
+++ Mentoria-Db.api/ApiDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mentoria_Db.api
+{
+    public class ApiDbContext : DbContext
+    {
+        public DbSet<Entidade1> Entidade1 { get; set; }
+
+
+        // Construtor que recebe as opções do DbContext
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+
+        }
+
+
+        // Método opcional para configurações adicionais do DbContext
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Aqui você pode adicionar configurações de entidade, relações, índices, etc.
+            // Exemplo:
+            modelBuilder.Entity<Entidade1>().HasKey(e => e.Id);
+        }
+    }
+
+    // Exemplo de entidade
+    public class Entidade1
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public DateTime DataCriacao { get; set; }
+    }
+
+}
---
+++ Mentoria-Db.api/Migrations/20241105164736_InitialMigration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mentoria_Db.api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Entidade1",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataCriacao = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entidade1", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Entidade1");
+        }
+    }
+}
